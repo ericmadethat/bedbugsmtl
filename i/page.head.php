7b72584bf87698d77b7ddb0d685fa47b0b2c38d8
frnkly <frank@frnk.ca>
2017-06-16 20:40:33 -0400

    Added tracking code

--- i/page.head.php
+++ i/page.head.php
@@ -18,4 +18,13 @@
 	<meta name="description" content="<?php echo __('Beat the Bug Description'); ?>" />
 	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 	<script type="text/javascript" src="<?php echo u('a/jquery.scrollTo.js'); ?>"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-97817609-2', 'auto');
+        ga('send', 'pageview');
+    </script>
 </head>
