96206cb4b1471d43f480cfd29bc01a2399b76480
frnkly <frank@frnk.ca>
2017-06-16 20:11:14 -0400

    Using https

--- i/page.head.php
+++ i/page.head.php
@@ -9,13 +9,13 @@
 	<link rel="apple-touch-icon" href="<?php echo u('a/logo.png'); ?>"/>
 	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo u('a/logo.76x76.png'); ?>"/>
 	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo u('a/logo.120x120.png'); ?>"/>
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Raleway" />
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway" />
 	<link rel="stylesheet" type="text/css" href="<?php echo u('a/style.css?') . MODIFIED_ON; ?>" />
 	<meta property="og:title" content="<?php echo __('Beat the Bug'); ?>" />
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content="http://bedbugsmtl.com/" />
+	<meta property="og:url" content="https://bedbugslanding.herokuapp.com/" />
 	<meta property="og:desc" content="<?php echo __('Beat the Bug Description'); ?>" />
 	<meta name="description" content="<?php echo __('Beat the Bug Description'); ?>" />
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 	<script type="text/javascript" src="<?php echo u('a/jquery.scrollTo.js'); ?>"></script>
 </head>
