da1ab0b4b9d254266cdb52d5877786d9b8a4b60a
Eric Nguyen <hello@ericdesigns.ca>
2017-06-19 18:29:08 -0400

    Update css tyle for launch section

--- i/page.launch.php
+++ i/page.launch.php
@@ -33,7 +33,7 @@
     <div id="mc_embed_signup">
     <form action="//herokuapp.us16.list-manage.com/subscribe/post?u=d2b9e529a8d5f98b13565292f&amp;id=4b9b74fd14" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <div id="mc_embed_signup_scroll">
-        <label for="mce-EMAIL">Avertissez moi quand c'est prêt, et gagnez un rabais de 30%</label>
+        <label for="mce-EMAIL">Avertissez moi quand c'est prêt, et obtenez un rabais de 30%</label>
         <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
         <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
         <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d2b9e529a8d5f98b13565292f_4b9b74fd14" tabindex="-1" value=""></div>
